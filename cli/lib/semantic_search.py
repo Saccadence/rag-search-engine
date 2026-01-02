036a4c2000588308eea4105b067686ea01995262
Saccadence <aurolfe035@gmail.com>
2026-01-02 11:43:06 -0600

    Functional Semantic Search

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -17,13 +17,13 @@ class SemanticSearch():
         self.documents = None
         self.document_map = {}
     
-    def generate_embedding(self, text):
+    def generate_embedding(self, text: str):
         if not text or text == '':
             raise ValueError("Text is empty")
 
         return self.model.encode([text])[0]
     
-    def build_embeddings(self, documents):
+    def build_embeddings(self, documents: dict):
         self.documents = documents
         doc_rep = []
         for doc in self.documents:
@@ -36,7 +36,7 @@ class SemanticSearch():
         
         return self.embeddings
     
-    def load_or_create_embeddings(self, documents):
+    def load_or_create_embeddings(self, documents: dict):
         self.documents = documents
         for doc in self.documents:
             self.document_map[doc["id"]] = doc
@@ -46,14 +46,47 @@ class SemanticSearch():
             if len(self.embeddings) == len(self.documents):
                 return self.embeddings
         return self.build_embeddings(documents)
+    
+    def search(self, query: str, limit: int) -> list:
+        if self.embeddings is None or self.documents is None:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
         
+        embedding = self.generate_embedding(query)
+        scores = []
+        for i in range(len(self.embeddings)):
+            doc_embed = self.embeddings[i]
+            doc = self.documents[i]
+            scores.append((cosine_similarity(embedding, doc_embed), doc))
+        scores.sort(key=lambda e: e[0], reverse=True)
+        
+        results = []
+        for score in scores:
+            results.append({
+                "score": score[0],
+                "title": score[1]["title"],
+                "description": score[1]["description"]
+            })
+            if len(results) == limit:
+                break
+        
+        return results
+
+def cosine_similarity(vec1, vec2) -> float:
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
 
 def verify_model() -> None:
     search = SemanticSearch()
     print(f"Model loaded: {search.model}")
     print(f"Max sequence length: {search.model.max_seq_length}")
     
-def embed_text(text):
+def embed_text(text: str) -> None:
     search = SemanticSearch()
     embedding = search.generate_embedding(text)
     
@@ -61,10 +94,17 @@ def embed_text(text):
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Dimensions: {embedding.shape[0]}")
 
-def verify_embeddings():
+def verify_embeddings() -> None:
     search = SemanticSearch()
     with open(ROOT / "data" / "movies.json", "r") as f:
         documents = json.load(f)["movies"]
     embeddings = search.load_or_create_embeddings(documents)
     print(f"Number of docs:   {len(documents)}")
     print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
+
+def embed_query_text(query: str) -> None:
+    search = SemanticSearch()
+    embedding = search.generate_embedding(query)
+    print(f"Query: {query}")
+    print(f"First 5 dimensions: {embedding[:5]}")
+    print(f"Shape: {embedding.shape}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -14,6 +14,13 @@ def main():
     embed_parser = subparsers.add_parser("embed_text", help="Embed text for model to use")
     embed_parser.add_argument("text", type=str, help="Text to embed")
     
+    embed_query_parser = subparsers.add_parser("embedquery", help="Embed a query for model to use")
+    embed_query_parser.add_argument("query", type=str, help="Query text to embed")
+    
+    search_parser = subparsers.add_parser("search", help="Search documents with a query and return formatted results")
+    search_parser.add_argument("query", type=str, help="Query to search against documents")
+    search_parser.add_argument("--limit", type=int, nargs='?', default=5, help="Limit for the number of results (Default = 5)")
+    
     args = parser.parse_args()
 
     match args.command:
@@ -25,6 +32,21 @@ def main():
             
         case "embed_text":
             embed_text(args.text)
+        
+        case "embedquery":
+            embed_query_text(args.query)
+            
+        case "search":
+            search = SemanticSearch()
+            with open(ROOT / "data" / "movies.json", "r") as f:
+                documents = json.load(f)["movies"]
+            search.load_or_create_embeddings(documents)
+            
+            results = search.search(args.query, args.limit)
+            for i in range(len(results)):
+                result = results[i]
+                print(f'{i}. {result["title"]} ({result["score"]})')
+                print(f'   {result["description"]}')
         
         case _:
             parser.print_help()
